d0cce491e9484b2b2c1eef9ec0a4debd49cf6669
chadrackitsia <itsia001@gmail.com>
2020-06-13 15:29:01 +0200

    initialisation du projet

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>Hello world</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data (){
+    return {
+      api_key: '2a715f2442a4c66455b6a1e2ce0d8ef9'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
